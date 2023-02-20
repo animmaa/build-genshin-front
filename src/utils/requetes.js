9e26a3d3d3dd65c9d29d4115275758f5167011fd
davy <davylebris@gmail.com>
2023-02-20 16:39:34 +0100

    fixe eslint

--- src/utils/requetes.js
+++ src/utils/requetes.js
@@ -5,11 +5,12 @@ import axios from 'axios';
 export const getNumberCard = async (choiceDeck, idCard) => {
   try {
     return await axios.get(
-      `http://localhost:8000/api/deck/number/${idCard}/${choiceDeck}`
+      `http://localhost:8000/api/deck/number/${idCard}/${choiceDeck}`,
     );
   } catch (error) {
     console.error(error);
   }
+  return null;
 };
 
 export const addCard = async (choiceDeck, idCard) => {
@@ -18,21 +19,23 @@ export const addCard = async (choiceDeck, idCard) => {
       `http://localhost:8000/api/card/addcardindeck/${choiceDeck}/${idCard}`,
       {
         card_id: idCard,
-      }
+      },
     );
   } catch (err) {
     console.error(err);
   }
+  return null;
 };
 
 export const deleteCard = async (choiceDeck, idCard) => {
   try {
     return await axios.delete(
-      `http://localhost:8000/api/card/deletecardindeck/${choiceDeck}/${idCard}`
+      `http://localhost:8000/api/card/deletecardindeck/${choiceDeck}/${idCard}`,
     );
   } catch (err) {
     console.error(err);
   }
+  return null;
 };
 
 /* requetes pour cardsListLog.jsx */
@@ -40,19 +43,21 @@ export const deleteCard = async (choiceDeck, idCard) => {
 export const getPersonnageNumberInTheDeck = async (choiceDeck) => {
   try {
     return await axios.get(
-      `http://localhost:8000/api/deck/totalpersonnage/${choiceDeck}`
+      `http://localhost:8000/api/deck/totalpersonnage/${choiceDeck}`,
     );
   } catch (err) {
     console.error(err);
   }
+  return null;
 };
 
 export const getNumberTotalInTheDeck = async (choiceDeck) => {
   try {
     return await axios.get(
-      `http://localhost:8000/api/deck/totalcard/${choiceDeck}`
+      `http://localhost:8000/api/deck/totalcard/${choiceDeck}`,
     );
   } catch (err) {
     console.error(err);
   }
+  return null;
 };
