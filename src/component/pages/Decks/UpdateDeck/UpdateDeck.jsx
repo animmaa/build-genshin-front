83db27912c7bcc003dde16f3f0d08fc5fc85eea9
davy <davylebris@gmail.com>
2023-05-05 16:54:44 +0200

    initialisation de la mise a jour des infos d'un deck

--- src/component/pages/Decks/UpdateDeck/UpdateDeck.jsx
+++ src/component/pages/Decks/UpdateDeck/UpdateDeck.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import './UpdateDeck.scss';
 
 const UpdateDeck = () => {
-  return (
-    <div>UpdateDeck</div>
-  )
-}
+  const [listCard, setListCard] = useState([]);
+  const getCardsList = async () => {
+    await axios
+      .get(`http://localhost:8000/api/card/personnage`)
+      .then((response) => {
+        setListCard(response.data);
+      })
+      .catch(() => {
+        navigator('/');
+      });
+  };
 
-export default UpdateDeck
+  useEffect(() => {
+    getCardsList();
+  }, []);
+
+  console.log(listCard);
+  return (
+    <div className="update_deck">
+      <div>
+        <h1>liste carte</h1>
+      </div>
+      {listCard.map((el) => (
+        <div className="zone_card" key={el.id}>
+          <img src={el.url} alt="" />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default UpdateDeck;
