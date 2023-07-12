066631975a7d438f365fb09b829f362b9db98148
davy <davylebris@gmail.com>
2023-07-12 14:43:08 +0200

    css de la liste des deck poster

--- src/component/Decks/allDeck/DescDeck.jsx
+++ src/component/Decks/allDeck/DescDeck.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { useLogin } from '../../../context/loginProvider';
+import './DescDeck.scss';
 
-function DescDeck({ deckName, idClick }) {
+function DescDeck({
+  deckName,
+  idClick,
+  deckImageTwo,
+  deckImageOne,
+  deckImageThree,
+}) {
   const { setIdDeck } = useLogin();
 
   const navigate = useNavigate();
@@ -14,9 +21,14 @@ function DescDeck({ deckName, idClick }) {
   };
 
   return (
-    <div className="deck_base">
-      <div className="card_deck">
+    <div className="deck_all_user">
+      <div className="name_deck">
         <h4>{deckName}</h4>
+      </div>
+      <div className="card_deck">
+        <img className="image1" src={deckImageOne} alt="" />
+        <img src={deckImageTwo} alt="" />
+        <img className="image3" src={deckImageThree} alt="" />
       </div>
       <button type="button" onClick={handleChoiceDeck}>
         voir le deck
@@ -28,6 +40,9 @@ function DescDeck({ deckName, idClick }) {
 DescDeck.propTypes = {
   deckName: PropTypes.string.isRequired,
   idClick: PropTypes.number.isRequired,
+  deckImageOne: PropTypes.string.isRequired,
+  deckImageTwo: PropTypes.string.isRequired,
+  deckImageThree: PropTypes.string.isRequired,
 };
 
 export default DescDeck;
